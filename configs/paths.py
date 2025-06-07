27eb3db80bd92b8b0f342e4ad890aed1137f0b01
wideraHannes <jowid100@gmail.com>
2025-06-07 20:00:10 +0200

    added ESM as encoding

--- configs/paths.py
+++ configs/paths.py
@@ -1,3 +1,13 @@
 from pathlib import Path
 
 project_root = Path(__file__).parent.parent.absolute()
+
+
+CROSSDOCKED = project_root / "crossdocked"
+
+EXAMPLE_PROTEIN = (
+    CROSSDOCKED
+    / "crossdocked_pocket10"
+    / "1A1C_MALDO_2_433_0"
+    / "1m4n_A_rec_1m7y_ppg_lig_tt_min_0_pocket10.pdb"
+)
